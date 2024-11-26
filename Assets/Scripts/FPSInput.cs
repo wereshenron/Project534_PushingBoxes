abd97bb1d1262ce6e1aea757cbf7ad5ed7f347da
Austin <austinduling@gmail.com>
2024-11-25 19:10:04 -0500

    Quick check in before I test transfer of prefabs

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 [AddComponentMenu("Control Script/FPS Input")]
 public class FPSInput : MonoBehaviour
 {
@@ -11,49 +11,79 @@ public class FPSInput : MonoBehaviour
     public float gravity = -9.8f;
     public float jumpForce = 8.0f;
     public float sprintBoost = 1.4f;
-    public float verticalVelocity;
-    private CharacterController _characterController;
-    
+
     private Rigidbody _rigidbody;
     private Camera _camera;
-    // Start is called before the first frame update
+    private Animator _animator;
+    private bool isGrounded = true;
+
     void Start()
     {
-        _characterController = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        _rigidbody.freezeRotation = true; // Prevent unwanted rotations due to physics
         _camera = GetComponentInChildren<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (_characterController.isGrounded)
+        // Handle Sprint
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            verticalVelocity = 0;
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                verticalVelocity = jumpForce;
-            }
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                speed *= sprintBoost;
-            }
-            else if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                speed = baseSpeed;
-            }
+            speed *= sprintBoost;
         }
-        transform.Translate(0, speed, 0);
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = baseSpeed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Get input for movement
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
 
-        movement = Vector3.ClampMagnitude(movement, speed);
-        verticalVelocity += gravity * Time.deltaTime;
-        movement.y = verticalVelocity;
+        // Apply gravity if not grounded
+        if (!isGrounded)
+        {
+            movement.y += gravity * Time.fixedDeltaTime;
+        }
 
-        movement *= Time.deltaTime;
-        movement = transform.TransformDirection(movement);
-        _characterController.Move(movement);
+        // Move Rigidbody
+        Vector3 velocity = transform.TransformDirection(movement) * Time.fixedDeltaTime;
+        _rigidbody.MovePosition(_rigidbody.position + velocity);
+
+        // Animate 
+        float rawSpeed = _rigidbody.velocity.magnitude;
+        float normalizedSpeed = Mathf.Clamp(rawSpeed / baseSpeed * sprintBoost, 0f, 1f); // Normalize based on max speed
+        _animator.SetFloat("Speed", normalizedSpeed);
+
+        // Handle Jump
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        // Check if grounded
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (Vector3.Dot(contact.normal, Vector3.up) > 0.5f) // Surface is mostly flat
+            {
+                isGrounded = true;
+                break;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // If no collisions, assume not grounded
+        isGrounded = false;
     }
 }
